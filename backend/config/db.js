a0347beec2e34ca61b9be863ee07c01639012fc3
poors17 <poorvajasaravananmdu@gmail.com>
2026-04-05 17:13:26 +0530

    fixed mongo connection for railway

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/bizbrain_ai");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected Successfully");
   } catch (error) {
     console.error("MongoDB Connection Failed", error.message);
