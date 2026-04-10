f7b89c54e2f61f1bb5c70f52ea672b0f6b95c94c
poors17 <poorvajasaravananmdu@gmail.com>
2026-04-10 16:52:21 +0530

    fixed port binding for railway

--- backend/server.js
+++ backend/server.js
@@ -33,6 +33,6 @@ app.get("/", (req, res) => {
 /* ================= SERVER ================= */
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
