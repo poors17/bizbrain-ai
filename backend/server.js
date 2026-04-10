9022058dcbf6386476a3fc060f60e3929577cef4
poors17 <poorvajasaravananmdu@gmail.com>
2026-04-10 16:55:34 +0530

    fix port to 3000

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.get("/", (req, res) => {
 });
 
 /* ================= SERVER ================= */
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
